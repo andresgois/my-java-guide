4e0d18c24203392d75b751b955a4a1932da594d2
Andre gois <andre.s.gois3@gmail.com>
2021-08-21 20:58:16 -0300

    exercicios das estruturas de repeticao

--- Curso_programacao/src/br/com/principal/Main.java
+++ Curso_programacao/src/br/com/principal/Main.java
@@ -5,15 +5,23 @@ import java.util.Scanner;
 
 public class Main {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Digite um número");
 		int x = sc.nextInt();
-		while(x > 1){
+		while(x >= 1){
 			System.out.println("Hello: "+x);
 			x--;
+		}
+		System.out.println("\n");
+		int i;
+		for(i=0; i < 5;i++) {
+			System.out.printf("[i] = %d\n",i);
 		}
 		
 		
