419d4c1bd7b0a634ad903ef397560458d0e50aae
Andre gois <andre.s.gois3@gmail.com>
2021-09-05 10:15:10 -0300

    trabalhando com datas - simpledateformat e calendar

--- Mod02/src/main/Program.java
+++ Mod02/src/main/Program.java
@@ -1,17 +1,26 @@
 package main;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.TimeZone;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		// TODO Auto-generated method stub
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
 		//matriz(sc);
-		matrizExer(sc);
+		//matrizExer(sc);
+		
+		//datas();
+		usandoCalendar();
 	}
 	
 	public static void matriz(Scanner sc) {
@@ -91,5 +100,67 @@ public class Program {
 		}
 	}
 
-	
+	public static void datas() throws ParseException {
+		SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
+		SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		SimpleDateFormat sdf3 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		sdf3.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
+		Date y1 = sdf1.parse("05/09/2021");
+		Date y2 = sdf2.parse("05/09/2021 09:48:07");
+		Date y3 = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
+		
+		Date x1 = new Date();
+		Date x2 = new Date(System.currentTimeMillis());
+		Date x3 = new Date(0L);
+		Date x4 = new Date(1000L * 60L * 60L * 5L);
+		
+		System.out.println("y1 sem format: "+y1);
+		System.out.println("y2 sem format: "+y2);
+		
+		System.out.println("y1 format: "+sdf2.format(y1));
+		System.out.println("y2 format: "+sdf2.format(y2));
+		
+		System.out.println("X1 - Hora Atual: "+sdf2.format(x1));
+		System.out.println("X2 - Hora Atual currentTimeMillis: "+sdf2.format(x2));
+		System.out.println("X3 - Horário inicial: "+sdf2.format(x3));
+		System.out.println("X4 - Horário inicial: "+sdf2.format(x4));
+		System.out.println("y2 format: "+sdf2.format(y3));
+		
+		System.out.println("-------------------------------------------");
+		/*
+		 * System.out.println("y1 sem format: "+y1);
+		 * System.out.println("y2 sem format: "+y2);
+		 */
+		
+		System.out.println("y1 format: "+sdf3.format(y1));
+		System.out.println("y2 format: "+sdf3.format(y2));
+		
+		System.out.println("X1 - Hora Atual: "+sdf3.format(x1));
+		System.out.println("X2 - Hora Atual currentTimeMillis: "+sdf3.format(x2));
+		System.out.println("X3 - Horário inicial: "+sdf3.format(x3));
+		System.out.println("X4 - Horário inicial: "+sdf3.format(x4));
+		System.out.println("y2 format: "+sdf3.format(y3));
+		
+		System.out.println("-------------------------------------------");
+	}
+
+	public static void usandoCalendar(){
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date d = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
+		Calendar cal = Calendar.getInstance();
+		
+		
+		System.out.println(sdf.format(d));
+		
+		cal.setTime(d);
+		cal.add(Calendar.HOUR_OF_DAY, 4);
+		d = cal.getTime();
+		int minutes = cal.get(Calendar.MINUTE);
+		int month = 1 + cal.get(Calendar.MONTH);
+		
+		System.out.println(sdf.format(d));
+		System.out.println("Minutes: "+minutes);
+		System.out.println("Month: "+month);
+	}
 }
