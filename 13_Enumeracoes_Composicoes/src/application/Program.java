ad402ef5fe69554ddfee3cba3385837c9701dd32
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-01-17 20:57:06 -0300

    :sparkles:	feat: test enums

--- 13_Enumeracoes_Composicoes/src/application/Program.java
+++ 13_Enumeracoes_Composicoes/src/application/Program.java
@@ -1,16 +1,23 @@
 package application;
 
-import java.util.Date;
-import application.entities.Order;
-import application.enums.OrderStatus;
-
 public class Program {
     
     public static void main(String[] args) {
-        
-        Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
-        
+       /* Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
         System.out.println(order);
+        // CONVERTER STRING PARA ENUM
+        OrderStatus os1 = OrderStatus.DELIVERED;
+        OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+        
+        System.out.println(os1);
+        System.out.println(os2);*/
+        
+        /**
+         * Ler os dados de um trabalhador com N contratos (N fornecido pelo usuário). Depois, solicitar 
+         * do usuário um mês e mostrar qual foi o salário do funcionário nesse mês, conforme exemplo 
+         * (próxima página).
+         */
+        
     }
     
 }
