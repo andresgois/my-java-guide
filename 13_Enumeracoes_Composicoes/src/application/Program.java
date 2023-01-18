be962450e14c69e1648a1efe53dd75079b1fd4ae
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-01-18 20:24:58 -0300

    :sparkles: feat: exercicio 1

--- 13_Enumeracoes_Composicoes/src/application/Program.java
+++ 13_Enumeracoes_Composicoes/src/application/Program.java
@@ -1,5 +1,14 @@
 package application;
 
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Scanner;
+
+import application.exercicio.enums.WorkerLevel;
+import application.exercicio.model.Department;
+import application.exercicio.model.HourContract;
+import application.exercicio.model.Worker;
+
 public class Program {
     
     public static void main(String[] args) {
@@ -17,7 +26,44 @@ public class Program {
          * do usuário um mês e mostrar qual foi o salário do funcionário nesse mês, conforme exemplo 
          * (próxima página).
          */
+        Scanner sc = new Scanner(System.in);
         
+        Worker w = null;
+        
+        System.out.print("Enter department's name: ");
+        String department = sc.next();
+        
+        System.out.println("Enter worker data: ");
+        System.out.print("Name: ");
+        String name = sc.next();
+        System.out.print("Level: ");
+        String level = sc.next();
+        System.out.print("Base Salary: ");
+        Double baseSalary = sc.nextDouble();
+        
+        System.out.print("How contracts to this worker? ");
+        int contracts = sc.nextInt();
+        
+        for (int i = 0; i < contracts; i++) {
+            
+            System.out.printf("Enter contract #%d data: \n",i+1);
+            System.out.print("Date (DD/MM/YYYY): ");
+            String data = sc.next();
+            System.out.print("Value per hour: ");
+            Double valuePerHour = sc.nextDouble();
+            System.out.print("Duration (hours): ");
+            Integer hours = sc.nextInt();
+            
+            HourContract contract = new HourContract(new Date(data), valuePerHour, hours);
+            WorkerLevel wl = WorkerLevel.valueOf(level);
+            w = new Worker(name, baseSalary, wl, new Department(department));
+            w.addContract(contract);
+        }
+        
+        System.out.println("Enter month and year to calculate income (MM/YYYY): ");
+        String mesAno = sc.next();
+        w.income(mesAno);
+        System.out.println(w);
     }
     
 }
--- 13_Enumeracoes_Composicoes/src/application/exercicio/model/Worker.java
+++ 13_Enumeracoes_Composicoes/src/application/exercicio/model/Worker.java
@@ -1,5 +1,7 @@
 package application.exercicio.model;
 
+import java.util.List;
+
 import application.exercicio.enums.WorkerLevel;
 
 public class Worker {
@@ -7,14 +9,21 @@ public class Worker {
     private String name;
     private Double baseSalary;
     private WorkerLevel level;
+    private Department department;
+    private List<HourContract> contract;
     
     public Worker() {}
 
-    public Worker(String name, Double baseSalary, WorkerLevel level) {
+    
+
+    public Worker(String name, Double baseSalary, WorkerLevel level,
+            Department department) {
         this.name = name;
         this.baseSalary = baseSalary;
         this.level = level;
+        this.department = department;
     }
+    
 
     public String getName() {
         return name;
@@ -38,6 +47,39 @@ public class Worker {
 
     public void setLevel(WorkerLevel level) {
         this.level = level;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+    
+    public void addContract(HourContract contract) {
+        this.contract.add(contract);
+    }
+    public void removeContract(HourContract contract) {
+        this.contract.remove(contract);
+    }
+    
+    public Double income(String mesAno) {
+        Integer year = retornaNum(mesAno, 1);
+        Integer month =  retornaNum(mesAno, 2);
+        Double valorContract = 0.0;
+        for (HourContract hourContract : contract) {
+            if(hourContract.getDate().getYear() == year && (hourContract.getDate().getMonth()+1) == month) {
+                valorContract += hourContract.getValuePerHour();
+            }
+        }
+        
+        return valorContract;
+    }
+    
+    public Integer retornaNum(String n, int posicaoRetorno) {
+        String[] arr = n.split("/");
+        return Integer.valueOf(arr[posicaoRetorno]);
     }
     
 }
