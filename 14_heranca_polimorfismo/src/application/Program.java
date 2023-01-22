221fd9d6a9de0f8e99539c1fa901952d8220d15e
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-01-22 15:10:41 -0300

    :sparkles: feat: exemplo 1 herança

---
+++ 14_heranca_polimorfismo/src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+public class Program {
+    
+    public static void main(String[] args) {
+        // #### Exemplo 1 ####
+        /**
+         * Classe Account é a *GENERALIZAÇÃO*
+         * Classe BusinessAccount é a *ESPECIALIZAÇÃO*
+         * BusinessAccount herda todos os atributos, propriedades e método que account tem.
+         */
+    }
+    
+}
